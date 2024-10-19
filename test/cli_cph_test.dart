9d6b065d076f94fe9a7469f921009887e689a612
jack meng <jackmeng0814@gmail.com>
2024-10-19 17:31:24 +0000

    reapply atl one test for https://github.com/exoad/console_judge/actions/runs/11419456694/job/31774051310

--- test/cli_cph_test.dart
+++ test/cli_cph_test.dart
@@ -1,4 +1,11 @@
+import "package:console_judge/app.dart";
+import "package:console_judge/util.dart";
+import "package:test/test.dart";
 
 void main() {
-
+  group("Esoteric", () {
+    test("Output Random name", () {
+      print(Util.getTempFileName("main.cxx", getExecFileExtension(Lang.cxx)));
+    });
+  });
 }
